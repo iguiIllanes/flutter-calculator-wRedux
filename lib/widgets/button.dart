7c206cf5349e39fa8fc2cfd7943b1e7ca755229e
Ignacio Illanes <igui10082002@gmail.com>
2022-08-21 01:38:54 -0400

    Add redux support

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,36 +1,50 @@
+import 'package:calculadora/redux/reducer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class CalcButton extends StatelessWidget {
   final String text;
   final Color primary;
   final Color onPrimary;
-  const CalcButton(
-      {Key? key,
-      required this.text,
-      this.primary = const Color(0xFF424242),
-      this.onPrimary = const Color(0xFFFFFFFF)})
-      : super(key: key);
+  final bool smallText;
+  final dynamic action;
+  // final void Function(Store<AppState> store)? converter;
+  const CalcButton({
+    Key? key,
+    required this.text,
+    this.primary = const Color(0xFF424242),
+    this.onPrimary = const Color(0xFFFFFFFF),
+    this.smallText = false,
+    this.action,
+  })
+  // this.converter})
+  : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ButtonTheme(
       height: 1000.0,
-      child: (ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          shape: const CircleBorder(),
-          padding: const EdgeInsets.all(20),
-          fixedSize: Size(80.0, 80.0),
-          primary: primary,
-          onPrimary: onPrimary,
-        ),
-        child: Text(
-          text,
-          style: const TextStyle(
-            fontSize: 35.0,
+      child: StoreConnector<AppState, VoidCallback>(
+        converter: (store) {
+          return () => store.dispatch(action);
+        }, //aqui
+        builder: (context, callback) => ElevatedButton(
+          onPressed: callback,
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            padding: const EdgeInsets.all(20),
+            fixedSize: const Size(80.0, 80.0),
+            primary: primary,
+            onPrimary: onPrimary,
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: smallText ? 25.0 : 35.0,
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
--- lib/widgets/display.dart
+++ lib/widgets/display.dart
@@ -1,13 +1,34 @@
+import 'package:calculadora/redux/reducer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 class Display extends StatelessWidget {
   const Display({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(10.0),
-      child: (Text("SEXOOOOOO")),
-    );
+    return SizedBox(
+        width: double.infinity,
+        height: 250.0,
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              StoreConnector<AppState, String>(
+                converter: (store) => store.state.operacion,
+                builder: (context, st) => Text(
+                  st,
+                  style: const TextStyle(fontSize: 50.0),
+                ),
+              ),
+              // Text(
+              //   "=",
+              //   style: TextStyle(fontSize: 40.0, color: Colors.greenAccent),
+              // )
+            ],
+          ),
+        ));
   }
 }
