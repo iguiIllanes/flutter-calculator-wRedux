59bfee2d8c25e492ab0a0d433a690524aba184e3
Ignacio Illanes <igui10082002@gmail.com>
2022-08-22 05:25:54 -0400

    Fix backspace in complex operations

--- lib/redux/handlers/edit_handler.dart
+++ lib/redux/handlers/edit_handler.dart
@@ -4,26 +4,27 @@ import '../reducer.dart';
 AppState editHandler(AppState previousState, dynamic action) {
   switch (action) {
     case Edit.AC:
-      return  AppState(
+      return AppState(
           currentNumber: 0,
           operacion: "",
           current: "",
           operations: previousState.operations);
     case Edit.Backspace:
-      String previousOperacion = previousState.operacion;
+      String previousOperacion = previousState.current;
       if (previousOperacion.length <= 1) {
         //TODO FIX
         return AppState(
             currentNumber: 0,
-            operacion: previousOperacion,
+            operacion: previousState.operacion
+                .substring(0, previousState.operacion.length - 1),
             current: "",
             operations: previousState.operations);
       }
       return AppState(
           currentNumber: num.parse(
               previousOperacion.substring(0, previousOperacion.length - 1)),
-          operacion:
-              previousOperacion.substring(0, previousOperacion.length - 1),
+          operacion: previousState.operacion
+              .substring(0, previousState.operacion.length - 1),
           current: previousOperacion.substring(0, previousOperacion.length - 1),
           operations: previousState.operations);
   }
